22722b0ddd799d5fa81a13e8506c33f51446158d
Jason Aravanis <j.aravanis@icloud.com>
2021-11-05 15:12:37 +1100

    Add mongoose models for users and articles

---
+++ models/Article.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+  title: { type: String, required: true, maxlength: 100 },
+  timestamp: { type: Date, required: true },
+  content: { type: String, required: true },
+  user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+});
+
+module.exports = mongoose.model("Article", ArticleSchema);
